1554b29e6c09fbf95ef653a0f5474781a021b723
quyen1102 <quyen.ttq.1102@gmail.com>
2021-11-14 20:53:14 +0700

    continue code

--- app/index.js
+++ app/index.js
@@ -1,8 +1,19 @@
-const btnBuys = document.querySelectorAll('.js-buy-ticket')
-const modal = document.querySelector('.modal')
-const btnCloseTicket = document.querySelector('.modal-close')
-const modalContainer = document.querySelector('.js-modal-container')
 
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const btnBuys = $$('.js-buy-ticket')
+const modal = $('.modal')
+const btnCloseTicket = $('.modal-close')
+const modalContainer = $('.js-modal-container')
+const btnMobileMenu = $('.mobile-menu-btn')
+const header = $('#header')
+const headerHeight = header.clientHeight
+const menuItems = $$('#nav li a[href*="#"]')
+
+// console.log(menuItems)
+
+// Buy ticket 
 btnBuys.forEach(buy => {
     buy.addEventListener('click', () =>{
        modal.classList.add('open')
@@ -20,3 +31,20 @@ modal.addEventListener('click', () => {
 modalContainer.addEventListener('click', (e) => {
     e.stopPropagation()
 })
+
+// Mobile-menu
+btnMobileMenu.addEventListener('click', () => {
+    let isClose = header.clientHeight === headerHeight
+   
+
+    if(isClose){
+            header.style.height = 'auto'
+    }else{
+            header.style.height = null
+    }
+})
+menuItems.forEach(item => {
+    item.onclick = () => {
+        header.style.height = null
+    }
+});
